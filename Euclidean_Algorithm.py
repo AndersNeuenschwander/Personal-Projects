5a28b4a1540329d83a06976ec6415940fd1ba2f6
AndersNeuenschwander <anders.rneuenschwander@gmail.com>
2025-10-16 14:21:10 -0600

    Update Euclidean_Algorithm.py

--- Euclidean_Algorithm.py
+++ Euclidean_Algorithm.py
@@ -1,3 +1,10 @@
+"""The Euclidean Algorithm is used to find the greatest common divisor (GCD) of any two numbers. 
+Takes as input two integers a and b satisfying a >= b >= 0. 
+If b = 0, output a, and terminate the algorithm. 
+If b != 0, divide a by b to get a = qb + r for some q,r in the integers (Z) with 0 <= r < b, 
+and then rerun the algorithm on the new integers b and r.
+"""
+
 import numpy as np
 
 
